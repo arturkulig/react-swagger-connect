4b200ba43144d9f98c1bcd3272ed306ce8ae89c5
Artur Kulig <arturkulig@gmail.com>
2018-09-14 12:04:36 +0200

    better differentation between op names

--- src/getOperationName.ts
+++ src/getOperationName.ts
@@ -2,9 +2,8 @@ import { Operation } from './schema';
 
 export function getOperationName(operation: Operation) {
   const { operationId = '' } = operation;
-  const [, opName] = /(.*?)Using([A-Z]+)/.exec(operationId) || [
-    null,
+  const [, opName, , postFix = ''] = /(.*?)Using([A-Z]+)(.*?)$/.exec(
     operationId,
-  ];
-  return opName!;
+  ) || [null, operationId];
+  return `${opName}${postFix}`;
 }
